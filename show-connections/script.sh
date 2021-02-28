8e0ba464775be570c07366d56ff5018a1fca3c8c
Timur Umarov <timur.dpr@gmail.com>
2021-02-28 11:36:58 +0300

    Add new feature - use sudo.
    * Add new parameter -s for using sudo

--- show-connections/script.sh
+++ show-connections/script.sh
@@ -12,6 +12,7 @@ Script will show established network connection or listening processes
      Example: -p firefox
  -n  Specify number of summary results
      Example: -n 10
+ -s  Use sudo
  -l  Show listening processes
 
   Example: ./script.sh -p firefox -n 10
@@ -32,6 +33,7 @@ do
 	flag+="o"
       shift ;;
     -h) help ;;
+    -s) sudo="sudo " ;;
     -l) flag+="l";;
      *) echo "$1 is not an option";;
   esac
@@ -41,6 +43,11 @@ done
 #main function
 getConnections () {
   # check minimal requirements varible
+ if [ -z "${sudo}" ]
+  then
+    echo "  You didn't use argument -s SUDO. Without sudo you can't see all connections"
+    sudo=''
+  fi 
   if [ -z "${proc}" ]
   then
     echo "  You didnt enter PID or name. By default will be show all connections"
@@ -52,11 +59,12 @@ getConnections () {
     results=5
   fi
 
+
   #if varible contains flag o (organization) then use whois
   if [[ ${flag} =~ "o" ]]
   then
     echo "  Count Organization"
-    requestIP=$(sudo netstat -tunp | awk -v proc=$proc '/'$proc'/ {print $5}' | cut -d: -f1)
+    requestIP=$(${sudo} netstat -tunp 2> /dev/null | awk -v proc=$proc '/'$proc'/ {print $5}' | cut -d: -f1)
     for ip in $requestIP
     do
       summ+=($(whois $ip | awk -F':' 'BEGIN{ORS="|"} /^OrgName/ {print $2;exit;}' ))
@@ -68,7 +76,7 @@ getConnections () {
   if [[ ${flag} =~ "l" ]]
   then
     echo "  Listening sockets"
-    sudo netstat -tunpl | awk -v proc=$proc '/'$proc'/ && /LISTEN/ {printf "%10s %10s %10s\n", $1,$4,$7}'| tail -n$results
+    ${sudo} netstat -tunpl 2> /dev/null | awk -v proc=$proc '/'$proc'/ && /LISTEN/ {printf "%10s %10s %10s\n", $1,$4,$7}'| tail -n$results
   fi
   exit
 }
