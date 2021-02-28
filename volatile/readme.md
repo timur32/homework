ee75198bc7d42031e40b1ea96a6ea6976745ded0
Timur Umarov <timur.dpr@gmail.com>
2021-02-28 20:54:06 +0300

    Add feature that accept number of month as argument.
    * Script expect numeric argument that defines month
    * Condition checks for correctness entered month
    * Obtain min and max year in source file
    * Changed main cycle for using above features

--- volatile/readme.md
+++ volatile/readme.md
@@ -1,6 +1,34 @@
 ## Script gets currency history from yandex, calculate valatile and etc.
 
+### Example: Calculate volatile for February
+````bash
+$ ./volatility.sh 2
 
+Calculate results for February.
+
+Mean for last 14 days: 89.6257
+
+Year: 2014 Min:         0  Max:            Mean:          
+Year: 2015 Min:     68.61  Max:    77.515  Mean:   72.8649
+Year: 2016 Min:     81.84  Max:    90.269  Mean:   85.8178
+Year: 2017 Min:      60.4  Max:     64.76  Mean:   62.1779
+Year: 2018 Min:   68.5725  Max:    71.505  Mean:   70.2216
+Year: 2019 Min:     74.09  Max:    75.275  Mean:    74.729
+Year: 2020 Min:      68.7  Max:     73.82  Mean:   69.9228
+Year: 2021 Min:     88.77  Max:    91.785  Mean:   89.9739
+
+Year Volatile
+2015 77.3174 
+2016 90.0323 
+2017 64.3579 
+2018 71.6878 
+2019 75.3215 
+2020 72.4828 
+2021 91.4814 
+
+Minimal volatile: 
+2017 64.3579 
+````
 ## Script description
 Download currency rates from yandex and save to file
 
--- volatile/volatility.sh
+++ volatile/volatility.sh
@@ -1,22 +1,51 @@
 #!/usr/bin/env bash
 
-curl -s https://yandex.ru/news/quotes/graph_2000.json > ./quotes.json
+#curl -s https://yandex.ru/news/quotes/graph_2000.json > ./quotes.json
 
+#set previos month by default
+month=$(date +%m -d '1 month ago')
+
+#Check requirements for enter correct month
+if [ -z "$1" ] || ! [[ "$1" =~ ^[0-9]*$ ]] || [ "$1" -gt 12 ] || [ "$1" -lt 1 ]
+then
+  echo -e "\nYou must enter correct number of Month. Default result will generate for $(date -d "$month/01" +%B)."
+  echo "Example: ./volatility.sh 03" 
+  echo -e "Example: ./volatility.sh 5"
+elif [[ "$1" =~ ^[0-9]+$ ]]
+then
+  if [[ "$1" =~ ^[0-9]$ ]]
+  then
+    month=0$1
+  else
+    month=$1
+  fi
+  echo -e "\nCalculate results for $(date -d "$month/01" +%B)".
+else
+  exit
+fi
 
 if [ -r quotes.json ]
 then
-  echo "Mean for last 14 days:" $(jq '.prices[][1]' quotes.json | tail -n 14 | awk -v mean=0 '{mean+=$1} END {print mean/14}')
+  #Calculate mean rate for last 2 weeks
+  echo -e "\nMean for last 14 days:" $(jq '.prices[][1]' quotes.json | tail -n 14 | awk -v mean=0 '{mean+=$1} END {print mean/14}')"\n"
+  
+  #Get min and max avalible year in file quotes.json
+  yearreq=$(jq -r '.prices[][0] |= (. / 1000 | strftime("%Y")) | .prices[] | .[0]' quotes.json)
+  minyear=$(echo "${yearreq[@]}"| head -n1)
+  maxyear=$(echo "${yearreq[@]}"| tail -n1)
 
-  for year in {2015..2020}
+  for year in $(seq $minyear $maxyear)
   do
-    jqreq=$(jq --arg Y $year --arg M "03" -r '.prices[][0] |= (. / 1000 | strftime("%Y%m")) | .prices[] | select(.[] == $Y+$M)| .[1]' quotes.json)
+    jqreq=$(jq --arg Y $year --arg M $month -r '.prices[][0] |= (. / 1000 | strftime("%Y%m")) | .prices[] | select(.[] == $Y+$M)| .[1]' quotes.json)
     min=$( echo "$jqreq" | sort | head -n1)
     max=$( echo "$jqreq" | sort | tail -n1)
     mean=$( echo "$jqreq" | awk -v mean=0 '{mean+=$1} END {print mean/NR}')
-    volatile+=$( awk -v mean="$mean" -v min="$min" -v max="$max" -v year="$year" 'BEGIN{print year ,((mean-min)+(mean+max))/2,"|"}' )   
+    volatile+=$( awk -v mean="$mean" -v min="$min" -v max="$max" -v year="$year" 'BEGIN{if (mean>0) print year ,((mean-min)+(mean+max))/2,"|"}' )   
     printf 'Year:%5s Min:%10s  Max:%10s  Mean:%10s\n' $year $min $max $mean
   done
-    echo -ne "\nMinimal volatile: "
+    printf "\nYear Volatile\n"
+    printf '%s\n' "${volatile[@]}"  | sed 's@|@\n@g'
+    printf "Minimal volatile: "
     printf '%s\n' "${volatile[@]}" | sed 's@|@\n@g' | sort -nk 2 | head -n 2
 else
   echo "File quotes.json not exists"
