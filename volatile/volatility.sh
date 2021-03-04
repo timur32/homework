9f907c48dbe0d4f2ec181f26f3267ba7609827b3
Timur Umarov <timur.dpr@gmail.com>
2021-03-04 22:07:35 +0300

    Long lines are splited for readability

--- volatile/volatility.sh
+++ volatile/volatility.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#curl -s https://yandex.ru/news/quotes/graph_2000.json > ./quotes.json
+curl -s https://yandex.ru/news/quotes/graph_2000.json > ./quotes.json
 
 #set previous month by default
 month=$(date +%m -d '1 month ago')
@@ -27,7 +27,8 @@ fi
 if [ -r quotes.json ]
 then
   #Calculate mean rate for last 2 weeks
-  echo -e "\nMean for last 14 days:" $(jq '.prices[][1]' quotes.json | tail -n 14 | awk -v mean=0 '{mean+=$1} END {print mean/14}')"\n"
+  echo -e "\nMean for last 14 days:" $(jq '.prices[][1]' quotes.json | tail -n 14 |\
+	   awk -v mean=0 '{mean+=$1} END {print mean/14}')"\n"
   
   #Get min and max avalible year in file quotes.json
   yearreq=$(jq -r '.prices[][0] |= (. / 1000 | strftime("%Y")) | .prices[] | .[0]' quotes.json)
@@ -36,15 +37,21 @@ then
 
   for year in $(seq $minyear $maxyear)
   do
-    jqreq=$(jq --arg Y $year --arg M $month -r '.prices[][0] |= (. / 1000 | strftime("%Y%m")) | .prices[] | select(.[] == $Y+$M)| .[1]' quotes.json)
+    #Get rates for specified month and year
+    jqreq=$(jq --arg Y $year --arg M $month -r '.prices[][0] |= (. / 1000 | strftime("%Y%m")) |
+	                                        .prices[] | select(.[] == $Y+$M)| .[1]' quotes.json)
+    #search min max rate and calculate volatile
     results+=$(echo "$jqreq" | awk -v year="$year" \
                                '{if (min=="") {min=$1; max=$1}; if($1>max) {max=$1}; if($1<min) {min=$1}} \
 	                        END { if (min!="") {printf "Year:%5s Min:%10s Max:%10s Volatile:%10s |",\
 			       	year, min, max, (max-min)/2}}')
   done
+  #Print results collected in cycle above
   printf '%s\n' "${results[@]}"  | sed 's@|@\n@g'
-#     printf "Minimal volatile: \n"
-printf '%s\n' "${results[@]}" | awk 'BEGIN {RS="|"} {{print $8};{if (min=="") {min=$8};if($8<min) {min=$8}} } END {print min}'
+  #Search min volatile and print it
+  printf '%s' "${results[@]}" | awk 'BEGIN {RS="|"} \
+                                {if (min=="") {min=$8;year=$2};if($8<min) {min=$8;year=$2}} \
+				END {printf "Minimal volatile in %s = %s\n",year,min}'
 else
   echo "File quotes.json not exists"
   exit
