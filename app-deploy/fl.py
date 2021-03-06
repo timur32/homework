46d3e21b5b466e63ec4273b8ffbee2bcab114bff
Timur Umarov <timur.dpr@gmail.com>
2021-03-06 20:46:03 +0300

    First version flask app

---
+++ app-deploy/fl.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from flask import Flask, request 
+import emoji, random
+app = Flask(__name__)
+
+def emojidecorate(word, count):
+    emoj = random.choice(emoji.emojize(':frowning::wink::mask::star::fire::alien:',use_aliases=True))
+    return emoj + (word + emoj) * int(count)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+#   print(request.is_json)
+ 
+    data = request.get_json(force=True)
+    return emojidecorate(data['word'], data['count'])
+
+app.run(debug=True, host='0.0.0.0', port = 5001)
+
