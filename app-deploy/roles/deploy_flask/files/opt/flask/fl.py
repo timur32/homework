8b8a0f91d5941c1be666f6a346dfa00f69a8cb99
Timur Umarov <timur.dpr@gmail.com>
2021-03-08 21:48:52 +0300

    Add ansible roles for deploy flask app.
    * Install Nginx service with basic configuration
    * Add proxypass config for Nginx
    * Copy flask app and requirements
    * Install pip and install pip-requirements for flask app
    * Create systemd service for flask app

--- app-deploy/roles/deploy_flask/files/opt/flask/fl.py
+++ app-deploy/roles/deploy_flask/files/opt/flask/fl.py
@@ -14,5 +14,6 @@ def index():
     data = request.get_json(force=True)
     return emojidecorate(data['word'], data['count'])
 
-app.run(debug=True, host='0.0.0.0', port = 5001)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port = 5001)
 
