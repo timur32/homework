8c65f2f7383b03e266df86da37716b4e6a91376f
Timur Umarov <timur.dpr@gmail.com>
2021-03-11 23:30:06 +0300

    Add simple error handling

--- app-deploy/roles/deploy_flask/files/opt/flask/fl.py
+++ app-deploy/roles/deploy_flask/files/opt/flask/fl.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
-from flask import Flask, request 
+from flask import Flask, request, render_template 
 import emoji, random
 app = Flask(__name__)
 
 def emojidecorate(word, count):
     emoj = random.choice(emoji.emojize(':frowning::wink::mask::star::fire::alien:',use_aliases=True))
     return emoj + (word + emoj) * int(count)
+
+curlanswer=("""
+This is flask service that recieve JSON object
+and return a string decorated with emoji
+
+
+
+""")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,9 +22,15 @@ def index():
       data = request.get_json(force=True)
       return emojidecorate(data['word'], data['count'])
     elif request.method == 'GET':
-      return 'You sent GET-request'
+      useragent = request.headers.get('User-Agent').lower()
+      print(useragent)
+      if useragent.find('curl'.lower()) >= 0:
+        return curlanswer
+      else:
+        return render_template('index.html')
     else:
-      return 'This service accept only GET or POST requests'
+      return 'This service accept only GET or POST json-requests'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port = 5001)
